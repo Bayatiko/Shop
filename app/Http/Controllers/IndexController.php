c1517821bb1fd40a08639199a3a9b35cbc3301fa
Tiko <33359644+Bayatiko@users.noreply.github.com>
2017-11-17 12:03:24 +0400

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
     public function takeAll(){
         $product = Product::all();
+        $category = Category::all();
         return view('index')
-        ->with('product',$product);
+        ->with('product',$product)
+        ->with('categorys',$category);
     }
 }
