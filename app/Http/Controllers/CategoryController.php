585bed5aa48e1e501a494c243b39679713cebe87
Tiko <33359644+Bayatiko@users.noreply.github.com>
2017-11-18 20:54:15 +0400

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Category;
-
 class CategoryController extends Controller
 {
     
-   public function index($id){
+    public function index($id){
         $product = Product::where('cat_id',$id)->get();
         $category = Category::all();
         return view('category')
