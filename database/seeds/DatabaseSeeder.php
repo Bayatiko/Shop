2858e3ac459146d0627121790bf63d457c13b1da
Tiko <33359644+Bayatiko@users.noreply.github.com>
2017-11-18 20:37:54 +0400

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,34 +12,131 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => str_random(10),
+            'name' => 'Name',
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
         ]);
 
         DB::table('products')->insert([
-            'name' => str_random(10),
-            'raiting' => random_int(1, 5),
-            'view' => random_int(1, 5),
-            'old price' => random_int(1, 5),
-            'price' => random_int(1, 5),
-            'anons' => str_random(10),
-            'text' => str_random(10),
-            'cat_id' => random_int(1, 4),
-            'img' => str_random(10).'.jpg',
+            'name' => 'Платье Paradise',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 500,
+            'price' => 400,
+            'anons' => 'Платье с карманами. Носите с пляжной обувью...',
+            'text' => ' Платье с карманами. Носите с пляжной обувью...',
+            'cat_id' => 2,
+            'img' => 'a9602c59a600256d93312acee27c23ac.jpg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Домашнее платье sOrnella',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 800,
+            'price' => 700,
+            'anons' => 'Хлопковое платье свободного кроя',
+            'text' => 'Хлопковое платье свободного кроя',
+            'cat_id' => 2,
+            'img' => 'fb769ee1dcfdf13dde2d34669467c537.jpg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Лонгслив Match',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 700,
+            'price' => 300,
+            'anons' => 'Мужской лонгслив силуэта slim. Выполнен из гипоаллергенного хлопка.',
+            'text' => 'Мужской лонгслив силуэта slim. Выполнен из гипоаллергенного хлопка.',
+            'cat_id' => 1,
+            'img' => '16db723236658bc9448357544fb9ca75.jpg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Футболка KANGAROOS',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 500,
+            'price' => 400,
+            'anons' => 'Прорезиненный принт-логотип Круглый вырез и короткие рукава',
+            'text' => 'Прорезиненный принт-логотип Круглый вырез и короткие рукава',
+            'cat_id' => 1,
+            'img' => '18674533_mmdb.jpg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Экран для проекторов',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 125,
+            'price' => 75,
+            'anons' => 'Формат видимой области экрана легко варьируется, так...',
+            'text' => 'Формат видимой области экрана легко варьируется, так...',
+            'cat_id' => 3,
+            'img' => '7269_img2.jpg '
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Магнитола Mystery BM-6008UB ',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 125,
+            'price' => 75,
+            'anons' => 'Эта компактная, удобная и очень простая в управлении модель...',
+            'text' => 'Эта компактная, удобная и очень простая в управлении модель....',
+            'cat_id' => 3,
+            'img' => '56631_img2.jpg '
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Диван кухонный',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 750,
+            'price' => 500,
+            'anons' => 'Формат видимой области экрана легко варьируется, так...',
+            'text' => 'Формат видимой области экрана легко варьируется, так...',
+            'cat_id' => 4,
+            'img' => 'img_9753_1.jpg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Комплект обеденный, стол + 4 стула',
+            'raiting' => 5,
+            'view' => 3,
+            'old_price' => 554,
+            'price' => 487,
+            'anons' => 'Формат видимой области экрана легко варьируется, так...',
+            'text' => 'Формат видимой области экрана легко варьируется, так...',
+            'cat_id' => 4,
+            'img' => '184601_2834_2734.jpg'
+        ]);
+       
+        DB::table('category')->insert([
+            
+            'title' => 'Men',
+         ]);
+         
+        DB::table('category')->insert([
+            'title' => 'Women',
         ]);
 
         DB::table('category')->insert([
-            'title' => str_random(10),
-            
+            'title' => 'Electronics',
         ]);
+
+        DB::table('category')->insert([
+            'title' => 'Furniture',
+        ]);
+
         DB::table('cart')->insert([
-            'user_id' => random_int(1, 5),
-            'product_id' => random_int(1, 5),
-            'product_price' => random_int(1, 5),
-            'product_title' => str_random(10),
-            'product_img' => str_random(10).'.jpg',
-            
+            'user_id' => 1,
+            'product_id' =>1,
+            'product_price' => 500,
+            'product_title' => 'mencloth',
+            'product_img' => '18674533_mmdb.jpg',
         ]);
+        
     }
 }
